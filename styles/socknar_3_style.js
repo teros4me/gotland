d1d80931ff572aa1ceb54787c6888fe84a48d7fc
Elijas <99867808+teros4me@users.noreply.github.com>
2023-10-08 15:45:06 +0200

    Updated ol-map

--- styles/socknar_3_style.js
+++ styles/socknar_3_style.js
@@ -21,7 +21,7 @@ var style_socknar_3 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(153,102,204,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(114,155,111,0.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(153,102,204,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}),fill: new ol.style.Fill({color: 'rgba(114,155,111,0.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
